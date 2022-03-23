0efaee6336740c1d81a695ac47884ecf14189f63
PolinaGuseva21 <pn_guseva@mail.ru>
2022-03-23 17:40:34 +0300

    Task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,41 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    let res = [];
+    for (let i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case 'I':
+                res.push(1);
+                break;
+            case 'V':
+                res.push(5);
+                break;
+            case 'X':
+                res.push(10);
+                break;
+            case 'L':
+                res.push(50);
+                break;
+            case 'C':
+                res.push(100);
+                break;
+            case 'D':
+                res.push(500);
+                break;
+            case 'M':
+                res.push(1000);
+                break;
+        }
+    }
+    while (res.length != 0) {
+        if (res[res.length - 1] > res[res.length - 2]) {
+            result += res[res.length - 1] - res[res.length - 2];
+            res.pop();
+            res.pop();
+        } else {
+            result += res[res.length - 1];
+            res.pop();
+        }
+    }
     return result;
 }
